94c4eecb8a44677fdcd3d69cdbfae6e39b6fc822
Anihithe <vjoly@inten.fr>
2019-04-02 16:14:50 +0200

    addFirebaseConfig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   constructor() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyBsIiyhHUN3ndthxkvM6DgotJ1FBm3kNf4',
+      authDomain: 'library-c3e6b.firebaseapp.com',
+      databaseURL: 'https://library-c3e6b.firebaseio.com',
+      projectId: 'library-c3e6b',
+      storageBucket: '',
+      messagingSenderId: '922361687752'
+    };
+    firebase.initializeApp(config);
+
   }
 
   ngOnInit() {
